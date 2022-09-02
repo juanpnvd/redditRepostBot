4743cf6d87744c3b00bafdbe917690e55c3c4c80
juanpnvd <103511484+juanpnvd@users.noreply.github.com>
2022-09-02 15:09:06 -0300

    Update bot_login.py

--- repostBot/bot_login.py
+++ repostBot/bot_login.py
@@ -6,7 +6,7 @@ def bot_login():
         print("logging in: ", os.getenv("reddit_username"))
         reddit = praw.Reddit(client_id = os.getenv("client_id"),
                     client_secret = os.getenv("client_secret"),
-                    user_agent = 'PRAW API tutorial Python Script',
+                    user_agent = os.getenv("user_agent"),
                     username = os.getenv("reddit_username"),
                     password = os.getenv("reddit_password"))   
         print("logged in!")
